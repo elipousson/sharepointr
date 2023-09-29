e9e148b02baa7182d9157a0a442afc1db92f22af
your name <eli.pousson@gmail.com>
2023-09-28 23:02:44 -0400

    feat: add get_sp_task

--- R/sp_plan.R
+++ R/sp_plan.R
@@ -66,6 +66,8 @@ list_sp_plans <- function(...,
     call = call
   )
 
+  check_ms(grp, "az_group", call = call)
+
   plan_list <- grp$list_plans(
     filter = filter,
     n = n %||% Inf
@@ -84,12 +86,42 @@ list_sp_plans <- function(...,
 
 #' Get a SharePoint task or list tasks from a planner
 #'
+#' [get_sp_task()] gets an individual planner task using the `get_task` method.
 #' [list_sp_tasks()] lists the tasks for a specified plan using the `list_tasks`
-#' method.
+#' method. Both default to returning a data frame.
 #'
 #' @name sp_tasks
 #' @seealso [Microsoft365R::ms_plan_task]
 NULL
+
+#' @rdname sp_tasks
+#' @name list_sp_tasks
+#' @param task_title,task_id Planner task title and id. Exactly one of
+#'   `task_title` and `task_id` must be supplied.
+#' @export
+get_sp_task <- function(task_title = NULL,
+                        task_id = NULL,
+                        ...,
+                        plan_title = NULL,
+                        plan_id = NULL,
+                        plan = NULL,
+                        as_data_frame = TRUE,
+                        call = caller_env()) {
+  plan <- plan %||%
+    get_sp_plan(plan_title, plan_id, ..., site = site, call = call)
+
+  check_ms(plan, "ms_plan")
+
+  check_exclusive_strings(task_title, task_id, call = call)
+
+  task <- plan$get_task(task_title = task_title, task_id = task_id)
+
+  if (!as_data_frame) {
+    return(task)
+  }
+
+  ms_obj_as_data_frame(task, obj_col = "ms_plan_task", call = call)
+}
 
 #' @rdname sp_tasks
 #' @name list_sp_tasks
@@ -105,10 +137,13 @@ list_sp_tasks <- function(plan_title = NULL,
                           ...,
                           filter = NULL,
                           n = NULL,
-                          site = NULL,
+                          plan = NULL,
                           as_data_frame = TRUE,
                           call = caller_env()) {
-  plan <- get_sp_plan(plan_title, plan_id, ..., site = site, call = call)
+  plan <- plan %||%
+    get_sp_plan(plan_title, plan_id, ..., call = call)
+
+  check_ms(plan, "ms_plan")
 
   task_list <- plan$list_tasks(
     filter = filter,
@@ -119,8 +154,5 @@ list_sp_tasks <- function(plan_title = NULL,
     return(task_list)
   }
 
-  ms_obj_list_as_data_frame(
-    task_list,
-    obj_col = "ms_plan_task"
-  )
+  ms_obj_list_as_data_frame(task_list, obj_col = "ms_plan_task", call = call)
 }

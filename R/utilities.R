63c5a6cb96e3928169e9b9566bbb1843ece9c0b7
your name <eli.pousson@gmail.com>
2023-09-30 17:37:36 -0400

    fix: correct handling of ms_obj by ms_obj_as_data_frame

--- R/utilities.R
+++ R/utilities.R
@@ -158,7 +158,7 @@ ms_obj_as_data_frame <- function(ms_obj,
                                  keep_list_cols = NULL,
                                  .name_repair = "universal_quiet",
                                  .error_call = caller_env()) {
-  if (has_name(ms_obj, "properties")) {
+  if (has_name(as.list(ms_obj), "properties")) {
     properties <- ms_obj$properties
   } else {
     properties <- ms_obj

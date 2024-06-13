e0d58fe71324c10df918673e9dfe7c4f24ce157c
your name <eli.pousson@gmail.com>
2024-06-12 22:06:59 -0400

    Add more URL helpers
    Also narrow scope for is_sp_site_url to avoid matching non-site URLs

--- R/is_sp_url.R
+++ R/is_sp_url.R
@@ -15,7 +15,14 @@ is_sp_url <- function(x) {
 #' @name is_sp_site_url
 #' @export
 is_sp_site_url <- function(x) {
-  is_sp_url(x) & grepl("\\.sharepoint.com/sites/", x)
+  is_sp_url(x) & grepl("\\.sharepoint.com/sites/[^/]+/?$", x)
+}
+
+#' @rdname is_sp_url
+#' @name is_sp_site_page_url
+#' @export
+is_sp_site_page_url <- function(x) {
+  is_sp_url(x) & grepl("/SitePages/", x)
 }
 
 #' @rdname is_sp_url
@@ -39,4 +46,13 @@ is_sp_type_url <- function(x, type = "w") {
 #' @export
 is_sp_folder_url <- function(x) {
   is_sp_type_url(x, type = "f")
+}
+
+
+#' @noRd
+is_sp_webview_list_url <- function(x) {
+  is_webview_list <- !is_sp_type_url(x, type = "l") &
+    grepl("/Lists/.+AllItems\\.aspx", x)
+
+  is_sp_url(x) & is_webview_list
 }

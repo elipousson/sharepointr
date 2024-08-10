4b4819baa9de32dc9e2187137453c645f2b1bb51
your name <eli.pousson@gmail.com>
2024-08-10 01:08:37 -0400

    Major refactor for update_sp_list_item
    Also add update_sp_list_items
    
    Also adds option to allow use of display name when creating or updating list items

--- R/sp_list_item.R
+++ R/sp_list_item.R
@@ -67,13 +67,11 @@ list_sp_list_items <- function(list_name = NULL,
     "Getting list items from SharePoint"
   )
 
-  sp_list$list_items(
-    filter = filter,
-    select = select,
-    all_metadata = all_metadata,
-    as_data_frame = as_data_frame,
-    pagesize = pagesize
-  )
+#' Pull a vector of display names named with corresponding column names
+#' @noRd
+pull_sp_list_display_names <- function(sp_list) {
+  sp_list_cols <- sp_list$get_column_info()
+  set_names(sp_list_cols$displayName, sp_list_cols$name)
 }
 
 
@@ -120,7 +118,7 @@ get_sp_list_item <- function(id,
   sp_list$get_item(id)
 }
 
-#' Creating and update list items
+#' Create or update list items
 #'
 #' @details Validation of data with with `create_sp_list_items()`
 #'
@@ -138,14 +136,15 @@ get_sp_list_item <- function(id,
 #' @param strict If `TRUE`, all column names in data must be matched to field
 #'   names in the supplied SharePoint list. If `FALSE` (default), unmatched
 #'   columns will be dropped with a warning.
-#' @param sync_fields If `TRUE`, use the `sync_fields` method to sync the fields
-#'   of the local sp_list object with the fields of the SharePoint List source.
 #' @param check_fields If `TRUE` (default), column names for the input data are
 #'   matched to the fields of the list object. If `FALSE`, the function will
 #'   error if any column names can't be matched to a field in the supplied
 #'   SharePoint list.
 #' @inheritParams ms_graph_arg_terms
 #' @inheritParams get_sp_list
+#' @param allow_display_nm If `TRUE`, allow data to use list field display names
+#'   instead of standard names. Note this requires a separate API call so may
+#'   result in a slower request.
 #' @examples
 #' sp_list_url <- "<SharePoint List URL with a Name field>"
 #'
@@ -163,6 +162,7 @@ create_sp_list_items <- function(data,
                                  list_id = NULL,
                                  sp_list = NULL,
                                  ...,
+                                 allow_display_nm = TRUE,
                                  site_url = NULL,
                                  site = NULL,
                                  drive_name = NULL,
@@ -185,6 +185,14 @@ create_sp_list_items <- function(data,
     drive = drive,
     call = call
   )
+
+  if (allow_display_nm) {
+    data <- replace_with_sp_list_display_names(
+      data,
+      .id = .id,
+      call = call
+    )
+  }
 
   if (check_fields) {
     data <- validate_sp_list_data_fields(
@@ -216,24 +224,28 @@ create_sp_list_items <- function(data,
 #' @noRd
 validate_sp_list_data_fields <- function(data,
                                          sp_list = NULL,
+                                         values = NULL,
                                          sync_fields = FALSE,
+                                         keep = "editable",
                                          strict = FALSE,
+                                         values_from = "name",
                                          call = caller_env()) {
-  check_data_frame(data, call = call)
+  if (is.null(values)) {
+    sp_list_meta <- get_sp_list_metadata(
+      sp_list = sp_list,
+      sync_fields = sync_fields,
+      keep = keep,
+      call = call
+    )
 
-  if (sync_fields) {
-    sp_list <- sp_list$sync_fields()
+    values <- sp_list_meta[[values_from]]
   }
-
-  sp_list_meta <- get_sp_list_metadata(sp_list = sp_list, call = call)
 
   nm <- names(data)
 
-  allowed_nm <- sp_list_meta[!sp_list_meta[["readOnly"]], ][["name"]]
+  nm_match <- nm %in% values
 
-  nm_match <- nm %in% allowed_nm
-
-  allowed_nm_msg <- "Field name{?s} from list are {.val {allowed_nm}}"
+  allowed_nm_msg <- "Field name{?s} from list are {.val {values}}"
 
   if (all(nm_match)) {
     return(data)
@@ -254,7 +266,8 @@ validate_sp_list_data_fields <- function(data,
   if (all(!nm_match)) {
     cli_abort(
       c(
-        "At least one column in {.arg data} must match field names from the supplied list.",
+        "At least one column in {.arg data} must match field names
+        from the supplied list.",
         "i" = allowed_nm_msg
       ),
       call = call
@@ -262,7 +275,8 @@ validate_sp_list_data_fields <- function(data,
   }
 
   if (any(!nm_match)) {
-    msg <- "All column names in {.arg data} must match field names from the supplied list."
+    msg <- "All column names in {.arg data} must match field names
+    from the supplied list."
 
     if (strict) {
       cli_abort(
@@ -280,43 +294,179 @@ validate_sp_list_data_fields <- function(data,
       )
     )
 
-    return(data[, nm_match, drop = FALSE])
+    if (is.data.frame(data)) {
+      data <- data[, nm_match, drop = FALSE]
+    } else {
+      data <- data[data]
+    }
+
+    return(data)
   }
 }
 
 #' @rdname create_sp_list_items
-#' @name update_sp_list_item
+#' @name update_sp_list_items
+#' @param .id Name of column in data to use for item ID values. Defaults to
+#'   "id".
 #' @export
-update_sp_list_item <- function(id,
+update_sp_list_items <- function(data,
+                                 list_name = NULL,
+                                 list_id = NULL,
+                                 sp_list = NULL,
+                                 ...,
+                                 .id = "id",
+                                 allow_display_nm = TRUE,
+                                 call = caller_env()) {
+  sp_list <- sp_list %||% get_sp_list(
+    list_name = list_name,
+    as_data_frame = FALSE,
+    list_id = list_id,
+    ...,
+    call = call
+  )
+
+  check_ms_obj(sp_list, "ms_list", call = call)
+
+  stopifnot(has_name(data, .id))
+
+  if (allow_display_nm) {
+    data <- replace_with_sp_list_display_names(
+      data,
+      .id = .id,
+      sp_list = sp_list,
+      call = call
+    )
+  }
+
+  update_data <- data
+  sp_item_id <- data[[.id]]
+  update_data[[.id]] <- NULL
+
+  imap(
+    sp_item_id,
+    \(id, n) {
+      update_sp_list_item(
+        .data = vctrs::vec_slice(
+          update_data,
+          i = n
+        ),
+        id = id,
+        sp_list = sp_list
+      )
+    }
+  )
+
+  invisible(data)
+}
+
+#' Replace names for a data frame or list with
+#' @noRd
+replace_with_sp_list_display_names <- function(data,
+                                               .id = "id",
+                                               sp_list = NULL,
+                                               values = NULL,
+                                               ...,
+                                               call = caller_env()) {
+  nm <- names(data)
+  list_display_nm <- values %||% pull_sp_list_display_names(sp_list)
+  nm_i <- match(nm, list_display_nm, incomparables = .id)
+  nm_match <- !is.na(nm_i)
+  nm[nm_match] <- names(list_display_nm)[nm_i[nm_match]]
+
+  .set_as_names(data, nm, repair = "check_unique", call = call)
+}
+
+#' @rdname create_sp_list_items
+#' @name update_sp_list_item
+#' @param .data A list or data frame with fields to update
+#' @export
+update_sp_list_item <- function(...,
+                                .data = NULL,
+                                id = NULL,
+                                sp_list_item = NULL,
+                                na_fields = c("drop", "replace"),
                                 list_name = NULL,
                                 list_id = NULL,
                                 sp_list = NULL,
-                                ...,
                                 site_url = NULL,
                                 site = NULL,
                                 drive_name = NULL,
                                 drive_id = NULL,
                                 drive = NULL,
                                 call = caller_env()) {
-  sp_list <- sp_list %||% get_sp_list(
-    list_name = list_name,
-    as_data_frame = FALSE,
-    list_id = list_id,
-    site_url = site_url,
-    site = site,
-    drive_name = drive_name,
-    drive_id = drive_id,
-    drive = drive,
-    call = call
-  )
+  .data <- .data %||% list2(...)
 
-  check_ms_obj(sp_list, "ms_list", call = call)
+  check_exclusive_args(id, sp_list_item, call = call)
 
-  cli::cli_progress_step(
-    "Updating item {.val {id}}"
-  )
+  # Check for a data frame provided with the ... argument
+  if (is.data.frame(.data[[1]]) && has_length(.data, 1)) {
+    cli_abort(
+      "You must use {.arg .data} to update a list item with a data frame.",
+      call = call
+    )
+  }
 
-  sp_list$update_item(id, ...)
+  if (is.data.frame(.data)) {
+    # FIXME: is it OK that .data supports both lists and data frames?
+    n_row_data <- nrow(.data)
 
-  invisible(id)
+    if (nrow(.data) > 1) {
+      cli_abort(
+        c(
+          "{.arg data} must be a single row, not {n_row_data}.",
+          "i" = "Use {.fn update_sp_list_items} to update a list
+          with a multi-row data frame."
+        ),
+        call = call
+      )
+    }
+
+    # Convert data frame to list
+    # FIXME: Double-check if this creates any issue when specifying values for
+    # more complex list field types
+    .data <- as.list(.data)
+  }
+
+  na_fields <- arg_match(na_fields, error_call = call)
+
+  if (na_fields == "drop") {
+    .data <- vctrs::vec_slice(
+      .data,
+      i = !is.na(.data)
+    )
+  }
+
+  if (is.null(sp_list_item) && !is.null(id)) {
+    sp_list <- sp_list %||% get_sp_list(
+      list_name = list_name,
+      as_data_frame = FALSE,
+      list_id = list_id,
+      site_url = site_url,
+      site = site,
+      drive_name = drive_name,
+      drive_id = drive_id,
+      drive = drive,
+      call = call
+    )
+
+    check_ms_obj(sp_list, "ms_list", call = call)
+
+    cli_progress_step(
+      "Updating item {.val {id}}"
+    )
+
+    inject(sp_list$update_item(id, !!!.data))
+  } else if (!is.null(sp_list_item)) {
+    check_ms_obj(sp_list_item, "ms_list_item", call = call)
+
+    id <- sp_list_item[["properties"]][["id"]]
+
+    cli_progress_step(
+      "Updating item {.val {id}}"
+    )
+
+    inject(sp_list_item$update(fields = list(!!!.data)))
+  }
+
+  invisible(.data)
 }

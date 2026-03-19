fec952413ac9e8241d324d835f61d12318a69e98
Eli Pousson <931136+elipousson@users.noreply.github.com>
2026-03-18 23:37:44 -0400

    Add lifecycle to package imports

--- R/sharepointr-package.R
+++ R/sharepointr-package.R
@@ -3,8 +3,9 @@
 
 ## usethis namespace: start
 #' @import rlang
-#' @importFrom glue glue
 #' @importFrom cli cli_progress_step cli_progress_message cli_abort
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
 #' @importFrom stringr str_match str_remove str_detect str_remove_all
 #'   str_split_i str_c
 ## usethis namespace: end

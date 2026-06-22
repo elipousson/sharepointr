78f2f219f80a30ff57df023a17462252358b70ec
Eli Pousson <931136+elipousson@users.noreply.github.com>
2026-06-22 17:06:13 -0400

    Add test for get_sp_list_column

--- tests/testthat/test-sp_list.R
+++ tests/testthat/test-sp_list.R
@@ -22,3 +22,41 @@ test_that("get_sp_list, get_sp_list_items, and get_sp_list_item works", {
     "data.frame"
   )
 })
+
+test_that("get_sp_list_column works", {
+  list_url <- "https://bmore.sharepoint.com/sites/DOP-ALL/Lists/DOP%20Employees/Staff%20Directory.aspx"
+
+  skip_if_no_ms_site(list_url)
+
+  sp_list <- get_sp_list(
+    list_url
+  )
+
+  sp_list_column <- get_sp_list_column(
+    column_name = "Notes",
+    sp_list = sp_list
+  )
+
+  expect_type(
+    sp_list_column,
+    "list"
+  )
+
+  expect_named(
+    sp_list_column,
+    c(
+      "@odata.context",
+      "columnGroup",
+      "description",
+      "displayName",
+      "enforceUniqueValues",
+      "hidden",
+      "id",
+      "indexed",
+      "name",
+      "readOnly",
+      "required",
+      "text"
+    )
+  )
+})
